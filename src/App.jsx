f49a126f17162e4ade4cabbdcab2a9d094eb596c
amiyakalo471-png <amiyakalo471@gmail.com>
2026-02-22 20:12:12 +0530

    Implemented React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import VoiceChat from "./pages/VoiceChat";
+import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 overflow-hidden">
-      <VoiceChat />
+      <Routes>
+        <Route path="/" element={<VoiceChat />} />
+      </Routes>
     </div>
   );
 }
